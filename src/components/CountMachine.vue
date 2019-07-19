0954aad97c21a1ce28878e99a477b1754a59e6cc
murphylee2048 <1134697131@qq.com>
2019-07-19 15:26:06 +0800

    create: CountMachine.vue

---
+++ src/components/CountMachine.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button v-on:click="decrease">-</button>
+        <span>{{count}}</span>
+        <button v-on:click="increase">+</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CountMachine",
+        data() {
+            return {count: 0}  // count 初始值为0
+        },
+        methods: {
+            decrease() {
+                this.count--;
+            },
+
+            increase() {
+                this.count++;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
